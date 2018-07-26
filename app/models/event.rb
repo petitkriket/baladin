f44719113e342143f57b562a76db3a30363abcc5
petitkriket <petitkriket@gmail.com>
2018-07-26 16:14:21 +0200

    geocoder events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :passenger
   belongs_to :user
-end
+  #
+  geocoded_by :address
+      reverse_geocoded_by :latitude, :longitude do |obj,results|
+      if geo = results.first
+          obj.city    = geo.city
+          obj.country = geo.country_code
+      end
+  end
+  after_validation :geocode, :reverse_geocode ,:if => :address_changed?
+
+  end
