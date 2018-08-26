ce1dffa22a24f82e726e20098c77a5a582da21da
petitkriket <petitkriket@gmail.com>
2018-08-26 21:35:55 +0200

    routing language

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|fr/ do
+#scope "(:locale)", locale: /en|fr/ do
+scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
     resources :events
     resources :contacts, only: [:new, :create]
     resources :passengers
     root to: 'passengers#index'
     devise_for :users, controllers: { registrations: 'users/registrations', confirmations: 'users/confirmations' }
     resources :users
-    get ":shortcut" => redirect("/users/sign_up?t=%{shortcut}"), constraints: lambda { |request| Passenger.where(shortcut: request[:shortcut]).any? },  id: :shortcut
+    get ":shortcut" => redirect("#{I18n.locale}/users/sign_up?t=%{shortcut}"), constraints: lambda { |request| Passenger.where(shortcut: request[:shortcut]).any? },  id: :shortcut
     #ok#get ':shortcut', to: 'events#new', constraints: lambda { |request| Passenger.where(shortcut: request[:shortcut]).any? }
-    #get ':shortcut', to: 'devise/registrations#new', constraints: lambda { |request| Passenger.where(shortcut: request[:shortcut]).any? }
   end
+
+    root to: redirect("/#{I18n.locale}", status: 302), as: :redirected_root
+    get "/*path", to: redirect("/#{I18n.locale}/%{path}", status: 302), constraints: { path: /(?!(#{I18n.available_locales.join("|")})\/).*/ }, format: false
 end
