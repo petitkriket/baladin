9c284cb5b28d86c91f5372434ef16c6c9dec9e6f
petitkriket <petitkriket@gmail.com>
2018-08-31 02:35:41 +0200

    mig email, reste domaine

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.assets.debug = true
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.phpnet.org",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
