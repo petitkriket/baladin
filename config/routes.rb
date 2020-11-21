0859f4832e5b34ec93413fcdf60b67ea25555815
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2020-11-21 21:50:33 +0100

    [Frontend] Webpack, tooling and linters (#16)
    * webpacker + vue 2
    * eslint
    * stylelint
    * storybook
    * bundle analyzer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/v3', to: 'home#index'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
