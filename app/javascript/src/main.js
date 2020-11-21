061169585fb83fd3e1b9a9b68118cd0e109a1a63
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2020-11-21 21:50:33 +0100

    [Frontend] Webpack, tooling and linters (#16)
    * webpacker + vue 2
    * eslint
    * stylelint
    * storybook
    * bundle analyzer

---
+++ app/javascript/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from '@/App.vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el = '#app';
+  const app = new Vue({
+    el,
+    render: (h) => h(App),
+  });
+  app.$mount();
+});
