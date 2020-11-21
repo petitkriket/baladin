932e16170baf3c6c9ae0ddf7ac0310c0a4a1249f
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2020-11-21 03:23:03 +0100

    suite

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,11 +7,11 @@
 Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
-    policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035', 'http://0.0.0.0:3035', 'ws://0.0.0.0:3035', 'https://*.tiles.mapbox.com', 'https://api.mapbox.com', 'https://events.mapbox.com'
+    policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035', 'http://0.0.0.0:3035', 'ws://0.0.0.0:3035'
   else
     policy.script_src :self, :https
-    policy.connect_src :self, :https, 'https://*.tiles.mapbox.com', 'https://api.mapbox.com', 'https://events.mapbox.com'
   end
+  policy.connect_src :self, 'https://*.tiles.mapbox.com', 'https://api.mapbox.com', 'https://events.mapbox.com'
 end
 
 # If you are using UJS then enable automatic nonce generation
@@ -20,4 +20,4 @@ end
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+Rails.application.config.content_security_policy_report_only = true
