64164943e4f5cd2b5a78eb793fcef0927058b8d0
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2021-11-06 14:42:02 +0100

    fix: email template routes

--- app/mailers/notif_mailer.rb
+++ app/mailers/notif_mailer.rb
@@ -2,6 +2,7 @@ class NotifMailer < ApplicationMailer
   default from: 'no-reply@' + Rails.application.credentials.dig(:domain_name_short),
           reply_to: 'contact@' + Rails.application.credentials.dig(:domain_name_short)
 
+  # an email to notify an administrator that a new user has registered an artwork
   def registration_email(user, admin)
     @user = user
     I18n.with_locale(admin.locale) do
@@ -12,6 +13,7 @@ class NotifMailer < ApplicationMailer
     end
   end
 
+    # an email to notify the new owner that contribution has been activated by site a administrator
   def event_activation_email(user, passenger, event)
     @user = user
     @passenger = passenger
@@ -25,6 +27,7 @@ class NotifMailer < ApplicationMailer
     end
   end
 
+  # an email to notify the user that previously owneed artwork has been activated by a following user
   def event_activation_previous_user_email(user, passenger, event)
     @user = user
     @passenger = passenger
